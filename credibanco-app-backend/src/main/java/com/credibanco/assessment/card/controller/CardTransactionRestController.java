c9bc7919a08b517bf16ad81ad8110b3d13e2d28a
diego_torres <dftorresj@unbosque.edu.co>
2022-02-03 06:52:02 -0500

    cross origin added

--- credibanco-app-backend/src/main/java/com/credibanco/assessment/card/controller/CardTransactionRestController.java
+++ credibanco-app-backend/src/main/java/com/credibanco/assessment/card/controller/CardTransactionRestController.java
@@ -2,12 +2,14 @@ package com.credibanco.assessment.card.controller;
 
 
 import java.util.Date;
+
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +25,7 @@ import com.credibanco.assessment.card.service.ICardTransactionService;
 import com.credibanco.assessment.card.service.IPurchaseService;
 import com.credibanco.assessment.card.service.IStatusService;
 
+@CrossOrigin(origins = "*",allowedHeaders = "*")
 @RestController
 public class CardTransactionRestController {
 
